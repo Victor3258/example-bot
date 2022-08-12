f068d11c61613a09103a06409c0674e95479fba5
Victor3258 <110357870+Victor3258@users.noreply.github.com>
2022-08-13 06:37:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,20 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  if (msg.content === '早安') {
+    msg.reply('早安！你有沒有吃早餐');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '午安') {
+    msg.reply('午安！你有沒有吃午餐');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '晚安') {
+    msg.reply('晚安！明天見');
   }
 });
 
